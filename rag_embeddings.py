36d8d429213f7908562cfc3bb8bc17cda5e3f82a
Jet Li <jet.li@ccrenew.com>
2026-07-15 15:39:06 -0400

    created embedding chunks

--- rag_embeddings.py
+++ rag_embeddings.py
@@ -2,6 +2,8 @@ from sentence_transformers import SentenceTransformer
 import chromadb
 import os
 
+# note: created embedding chunks, now time to continue RAG
+
 def create_embeddings():
     # Load the embedding model
     #model = SentenceTransformer("BAAI/bge-base-en-v1.5")
@@ -13,24 +15,33 @@ def create_embeddings():
         with open(file_path, "r") as r:
             lines = r.readlines()
             file_str = ""
+            id_description = []
+            severity = ""
             for line in lines:
                 if separator in line:
                     file_str += "##"
+                    id_description.append(line.split(" ")[1])
+                elif "**Severity:**" in line: 
+                    severity = line.lstrip("#").strip()
                 else:
-                    file_str += line.strip()
+                    file_str += line.strip() + "\n"
 
             
             splitted = file_str.split("##")
-            for i in range(1, len(splitted)):
-                # fix this 
-                id = filename
+            for i in range(1, len(splitted)): 
+                section = id_description[i - 1]
+
+                id = filename + "#" + section
                 text = splitted[i]
                 metadata = {
                     # fix this 
+                    "source_file": filename,
                     "doc_id": id,
-                    "section":
+                    "section": section,
+                    "severity": severity 
                 }
-                print(splitted[i] + "\n")
+                chunk = [id, text, metadata]
+                print(chunk)
 
 
 if __name__ == "__main__":
